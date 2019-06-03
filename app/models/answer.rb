20e6b0935316eef8304c9197c4e807da494a2e13
Gleb Grishakov <glebson1988@yandex.ru>
2019-06-03 08:53:01 +0300

    homework of lesson6 att2

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,13 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :correct, -> { where(correct: true) }
   validates :body, presence: true
   validate :validate_answers_limit
+
+  scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_answers_limit
-    errors.add(:question) if question.answers.size >= 4
+    errors.add(:question, 'Ответов не должно быть больше 4') if question.answers.count >= 4
   end
 end

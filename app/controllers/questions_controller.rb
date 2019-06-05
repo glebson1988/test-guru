77dfc8825f494dcf2c631ce05e7b800e85ab4417
Gleb Grishakov <glebson1988@yandex.ru>
2019-06-05 11:55:19 +0300

    homework of lesson7

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,42 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render inline: 'Вопросы теста: <%= @test.questions.pluck(:body) %>'
+  end
+
+  def show
+    render inline: 'Вопрос: <%= @question.body %>'
+  end
+
+  def new; end
+
+  def create
+    @question = Question.create(question_params)
+  end
+
+  def destroy
+    @question.destroy
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Вопрос не найден!'
+  end
+end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,7 +6,7 @@ class Test < ApplicationRecord
   has_many :users, through: :passed_tests
 
   validates :title, presence: true,
-            uniqueness: { scope: :level }
+                    uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   scope :easy, -> { where(level: 0..1) }

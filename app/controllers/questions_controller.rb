9dd573244c73781d8ca23b60bf5651cd551de00c
Gleb Grishakov <glebson1988@yandex.ru>
2019-06-06 07:25:40 +0300

    homework of lesson7 att2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,20 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index create new]
-  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    render inline: 'Вопросы теста: <%= @test.questions.pluck(:body) %>'
+    render inline: 'Вопросы теста: <%= @test.questions.inspect %>'
   end
 
   def show
-    render inline: 'Вопрос: <%= @question.body %>'
+    render inline: 'Вопрос: <%= Question.find(params[:id]).inspect %>'
   end
 
   def new; end
 
   def create
-    @question = Question.create(question_params)
+    @question = @test.questions.create!(question_params)
   end
 
   def destroy
@@ -26,10 +25,6 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
-  end
-
-  def find_question
-    @question = Question.find(params[:id])
   end
 
   def question_params

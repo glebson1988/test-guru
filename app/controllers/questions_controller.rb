a17043db6be04ffc4060f3ed5d02e3643535c719
Gleb Grishakov <glebson1988@yandex.ru>
2019-06-09 02:38:46 +0300

    homework of lesson8

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,30 +1,48 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy edit update]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-    render inline: 'Вопросы теста: <%= @test.questions.inspect %>'
-  end
+  def show; end
 
-  def show
-    render inline: 'Вопрос: <%= Question.find(params[:id]).inspect %>'
+  def new
+    @question = Question.new
   end
-
-  def new; end
 
   def create
-    @question = @test.questions.create!(question_params)
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
   end
 
   def destroy
     @question.destroy
+    redirect_to @question.test
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def question_params

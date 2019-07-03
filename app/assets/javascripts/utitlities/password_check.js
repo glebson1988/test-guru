71f6ec9a9ad98e73f9f526529519c7fd469099c2
Gleb Grishakov <glebson1988@yandex.ru>
2019-07-03 22:12:41 +0300

    homework of lesson15 att2

--- app/assets/javascripts/utitlities/password_check.js
+++ app/assets/javascripts/utitlities/password_check.js
@@ -1,28 +1,38 @@
-document.addEventListener('turbolinks:load', () => {
-    let control = document.querySelector('.check-pass');
+document.addEventListener('turbolinks:load', function () {
+    var control = document.querySelector('.check-pass');
 
     if (control) { control.addEventListener('input', passwordCheck) }
 });
 
-let passwordCheck = () => {
+function passwordCheck() {
+    var password = document.getElementById('user_password');
+    var confirmation = document.getElementById('user_password_confirmation');
+    var match = document.querySelector('.octicon-check').classList;
+    var notMatch = document.querySelector('.octicon-x').classList;
 
-    let password = document.getElementById('user_password');
-    let confirmation = document.getElementById('user_password_confirmation');
+    if (confirmation.value) {
+        if (password.value === confirmation.value) {
+            match.remove('hide');
+            notMatch.add('hide');
+            password.classList.add('border-green');
+            confirmation.classList.add('border-green');
+            password.classList.remove('border-red');
+            confirmation.classList.remove('border-red');
+        } else {
+            match.add('hide');
+            notMatch.remove('hide');
+            password.classList.remove('border-green');
+            confirmation.classList.remove('border-green');
+            password.classList.add('border-red');
+            confirmation.classList.add('border-red');
+        }
 
-    if (password.value === '' || confirmation.value === '') {
-    } else if (password.value === confirmation.value) {
-        document.querySelector('.octicon-check').classList.remove('hide');
-        document.querySelector('.octicon-x').classList.add('hide');
-        password.classList.add('border-green');
-        confirmation.classList.add('border-green');
+    } else {
+        match.add('hide');
+        notMatch.add('hide');
         password.classList.remove('border-red');
         confirmation.classList.remove('border-red');
-    } else {
-        document.querySelector('.octicon-x').classList.remove('hide');
-        document.querySelector('.octicon-check').classList.add('hide');
-        password.classList.remove('border-green');
-        confirmation.classList.remove('border-green');
-        password.classList.add('border-red');
-        confirmation.classList.add('border-red');
     }
-};
+}
+
+
